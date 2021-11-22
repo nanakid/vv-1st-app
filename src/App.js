9839b511205a3d9563043c3b0b378a28d908854b
Maruko Kuriyama <maruko.kuriyama@gmail.com>
2021-11-22 13:45:39 -0800

    update app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const test = 'foo'
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          VV's 1st React App! {test}
         </p>
         <a
           className="App-link"
